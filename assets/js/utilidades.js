d20ba8b7341b8ffa5d82f2e675aa137010e4af0b
diego <d.riosrojas007@gmail.com>
2020-11-01 10:41:11 -0300

    correciones

--- assets/js/utilidades.js
+++ assets/js/utilidades.js
@@ -174,8 +174,8 @@ $(document).on("change", 'input[type="file"]', function () {
 			this.files[0].name = "";
 			break;
 	}
-	if (fileSize > 15000000) {
-		alert("El archivo tiene que pesar menos de 15mb");
+	if (fileSize > 2100000) {
+		alert("El archivo tiene que pesar menos de 2mb");
 		this.value = ""; // reset del valor
 		this.files[0].name = "";
 	}
