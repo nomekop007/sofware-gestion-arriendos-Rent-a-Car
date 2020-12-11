c448308a982f660b2b4fadaa0a86ff22622467bd
diego <d.riosrojas007@gmail.com>
2020-12-10 22:48:55 -0300

    agregacion de mensaje de actualizacion

--- application/views/content/view_module/views_atencion/index_atencion.php
+++ application/views/content/view_module/views_atencion/index_atencion.php
@@ -1,13 +1,12 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-    <div
-        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Modulos Atencion</h1>
-    </div>
-    <div>
-        <h5>Bienvenido <?php echo $this->session->userdata('nombre'); ?> </h5>
-        <img style="width:15%" src="<?php echo base_route() ?>assets/images/logo3.png" />
-        <img style="width:40%;margin: 40px" src="<?php echo base_route() ?>assets/images/logo.png" />
-    </div>
+	<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+		<h1 class="h2">Modulos Atencion</h1>
+	</div>
+	<div>
+		<h5>Bienvenido <?php echo $this->session->userdata('nombre'); ?> </h5>
+		<img style="width:10%" src="<?php echo base_route() ?>assets/images/logo3.png" />
+		<img style="width:30%;margin: 40px" src="<?php echo base_route() ?>assets/images/logo.png" />
+	</div>
 </main>
 
 
--- application/views/content/view_module/views_gestion/index_gestion.php
+++ application/views/content/view_module/views_gestion/index_gestion.php
@@ -1,22 +1,30 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-    <div
-        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Modulos de Gestion</h1>
-    </div>
-    <div>
-        <h5>Bienvenido <?php echo $this->session->userdata('nombre'); ?> </h5>
-        <div class="row">
-            <div class="col-md-12">
-                <br>
-                <img style="width:15%" src="<?php echo base_route() ?>assets/images/logo3.png" />
-                <img style="width:40%;margin: 40px" src="<?php echo base_route() ?>assets/images/logo.png" />
-            </div>
-        </div>
-        <div id="card_alertas"></div>
-
-    </div>
-    </div>
-    <br><br>
+	<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+		<h1 class="h2">Modulos de Gestion</h1>
+	</div>
+	<div>
+		<h5>Bienvenido <?php echo $this->session->userdata('nombre'); ?> </h5>
+		<div class="row">
+			<div class="col-md-12">
+				<br>
+				<img style="width:10%" src="<?php echo base_route() ?>assets/images/logo3.png" />
+				<img style="width:30%;margin: 40px" src="<?php echo base_route() ?>assets/images/logo.png" />
+			</div>
+		</div>
+		<div class="card  text-success">
+			<div class="m-4">
+				<h4>Sistema actualizado!</h4>
+				<span>Cambios realizados:</span>
+				<li>Se modifico la interfaz de la ventana mostrar arriendo</li>
+				<li>Se agrego validacion de los archivos requeridos (garantia,documentos) en mostrar arriendo</li>
+				<li>Se modifico la interfaz del pago de los arriendos </li>
+				<li>se agrego nuevo modulo gestion de daños de vehiculos</li>
+			</div>
+		</div>
+		<div id="card_alertas"></div>
+	</div>
+	</div>
+	<br><br>
 </main>
 
 
@@ -27,73 +35,73 @@
 
 
 <script>
-(cargarArriendosActivos = async () => {
-    const data = new FormData();
-    data.append("filtro", "ACTIVO");
-    const response = await ajax_function(data, "cargar_arriendos");
-    if (response.success) {
-        $.each(response.data, (i, arriendo) => {
-            temporizador(arriendo, arriendo.id_arriendo);
-        });
-    }
-})();
+	(cargarArriendosActivos = async () => {
+		const data = new FormData();
+		data.append("filtro", "ACTIVO");
+		const response = await ajax_function(data, "cargar_arriendos");
+		if (response.success) {
+			$.each(response.data, (i, arriendo) => {
+				temporizador(arriendo, arriendo.id_arriendo);
+			});
+		}
+	})();
 
-const temporizador = ({
-    fechaRecepcion_arriendo,
-    diasActuales_arriendo
-}, id_arriendo) => {
-    const countDownDate = moment(fechaRecepcion_arriendo);
+	const temporizador = ({
+		fechaRecepcion_arriendo,
+		diasActuales_arriendo
+	}, id_arriendo) => {
+		const countDownDate = moment(fechaRecepcion_arriendo);
 
-    let time = countDownDate.diff(moment());
-    // 1 segundo = 1.000
-    // 1 minuto = 60.000
-    // 1 hora = 3.600.000
-    if (diasActuales_arriendo < 2 && time <= 3600000 * 5) {
-        let fila = `
+		let time = countDownDate.diff(moment());
+		// 1 segundo = 1.000
+		// 1 minuto = 60.000
+		// 1 hora = 3.600.000
+		if (diasActuales_arriendo < 2 && time <= 3600000 * 5) {
+			let fila = `
             <div id="alert${id_arriendo}" class="alert  alert-dismissible fade show" role="alert">
                ARRIENDO Nº${id_arriendo} <div id=time${id_arriendo}> </div>
                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
              <span aria-hidden="true">&times;</span>
             </button>
             </div> `;
-        $("#card_alertas").append(fila);
-        alertaTemporizador(countDownDate, time, id_arriendo)
-        time = setInterval(function() {
-            alertaTemporizador(countDownDate, time, id_arriendo)
-        }, 1000);
-    }
+			$("#card_alertas").append(fila);
+			alertaTemporizador(countDownDate, time, id_arriendo)
+			time = setInterval(function() {
+				alertaTemporizador(countDownDate, time, id_arriendo)
+			}, 1000);
+		}
 
 
-    if (diasActuales_arriendo >= 2 && time <= 3600000 * 12) {
-        let fila = `
+		if (diasActuales_arriendo >= 2 && time <= 3600000 * 12) {
+			let fila = `
             <div id="alert${id_arriendo}" class="alert  alert-dismissible fade show" role="alert">
                ARRIENDO Nº${id_arriendo} <div id=time${id_arriendo}> </div>
                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
              <span aria-hidden="true">&times;</span>
             </button>
             </div> `;
-        $("#card_alertas").append(fila);
-        alertaTemporizador(countDownDate, time, id_arriendo)
-        time = setInterval(function() {
-            alertaTemporizador(countDownDate, time, id_arriendo)
-        }, 1000);
-    }
-};
+			$("#card_alertas").append(fila);
+			alertaTemporizador(countDownDate, time, id_arriendo)
+			time = setInterval(function() {
+				alertaTemporizador(countDownDate, time, id_arriendo)
+			}, 1000);
+		}
+	};
 
 
-const alertaTemporizador = (countDownDate, time, id_arriendo) => {
-    let diff = countDownDate.diff(moment());
-    if (diff <= 0) {
-        clearInterval(time);
-        // If the count down is finished, write some text
-        $(`#time${id_arriendo}`).text("este arriendo a EXPIRADO");
-        $(`#time${id_arriendo}`).addClass("text-danger");
-        $(`#alert${id_arriendo}`).addClass("alert-danger");
-    } else {
-        $(`#alert${id_arriendo}`).addClass("alert-warning");
-        $(`#time${id_arriendo}`).text(
-            "queda menos de " + moment.utc(diff).format(" HH:mm:ss") + " horas para expirar"
-        );
-    }
-}
+	const alertaTemporizador = (countDownDate, time, id_arriendo) => {
+		let diff = countDownDate.diff(moment());
+		if (diff <= 0) {
+			clearInterval(time);
+			// If the count down is finished, write some text
+			$(`#time${id_arriendo}`).text("este arriendo a EXPIRADO");
+			$(`#time${id_arriendo}`).addClass("text-danger");
+			$(`#alert${id_arriendo}`).addClass("alert-danger");
+		} else {
+			$(`#alert${id_arriendo}`).addClass("alert-warning");
+			$(`#time${id_arriendo}`).text(
+				"queda menos de " + moment.utc(diff).format(" HH:mm:ss") + " horas para expirar"
+			);
+		}
+	}
 </script>
