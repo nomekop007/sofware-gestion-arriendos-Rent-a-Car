665cc99e971b3da4766a8d19596a732e7a4f02cb
diego <d.riosrojas007@gmail.com>
2020-12-11 21:33:51 -0300

    hidden mensaje

--- application/views/content/view_module/views_gestion/index_gestion.php
+++ application/views/content/view_module/views_gestion/index_gestion.php
@@ -11,7 +11,7 @@
 				<img style="width:30%;margin: 40px" src="<?php echo base_route() ?>assets/images/logo.png" />
 			</div>
 		</div>
-		<div class="card  text-success">
+		<div hidden class="card  text-success">
 			<div class="m-4">
 				<h4>Sistema actualizado!</h4>
 				<span>Cambios realizados:</span>
