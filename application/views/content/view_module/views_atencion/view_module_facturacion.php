b0214bbf76063348dd7cc033dd0bbdc15c6fde9c
diego rios rojas <d.riosrojas007@gmail.com>
2020-12-14 16:23:45 -0300

    correcion

--- application/views/content/view_module/views_atencion/view_module_facturacion.php
+++ application/views/content/view_module/views_atencion/view_module_facturacion.php
@@ -26,120 +26,119 @@
                 <br><br>
                 <form class="needs-validation" novalidate id="form_registrar_factura">
                     <div class="form-row">
-                        <div class="container card">
-                            <br>
-                            <div class="container">
-                                <h6>Facturacion</h6>
-                                <div class="form-row card-body">
-                                    <div class="custom-control custom-radio custom-control-inline ">
-                                        <input type="radio" value="BOLETA" id="radioBoleta" name="customRadio1"
-                                            class="custom-control-input" checked>
-                                        <label class="custom-control-label" for="radioBoleta">Boleta</label>
-                                    </div>
-                                    <div class="custom-control custom-radio custom-control-inline ">
-                                        <input type="radio" value="FACTURA" id="radioFactura" name="customRadio1"
-                                            class="custom-control-input">
-                                        <label class="custom-control-label" for="radioFactura">Factura</label>
-                                    </div>
+                        <br>
+                        <div class="container">
+                            <h6>Facturacion</h6>
+                            <div class="form-row card-body">
+                                <div class="custom-control custom-radio custom-control-inline ">
+                                    <input type="radio" value="BOLETA" id="radioBoleta" name="customRadio1"
+                                        class="custom-control-input" checked>
+                                    <label class="custom-control-label" for="radioBoleta">Boleta</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline ">
+                                    <input type="radio" value="FACTURA" id="radioFactura" name="customRadio1"
+                                        class="custom-control-input">
+                                    <label class="custom-control-label" for="radioFactura">Factura</label>
                                 </div>
                             </div>
-                            <div class="container">
-                                <div class="form-row card-body">
-                                    <div class="form-group col-xl-4">
-                                        <label for="inputNumFacturacion">Numero comprobante</label>
-                                        <input maxLength="20" id="inputNumFacturacion" name="inputNumFacturacion"
-                                            type="number" class="form-control" placeholder="Nº Boleta/Factura" required>
-                                    </div>
-                                    <div class="form-group col-xl-6">
-                                        <label for="inputFileFacturacion">Comprobante</label>
-                                        <input accept="image/x-png,image/gif,image/jpeg,image/jpg,application/pdf"
-                                            type="file" class="form-control-file" id="inputFileFacturacion"
-                                            name="inputFileFacturacion" required>
-                                    </div>
+                        </div>
+                        <div class="container">
+                            <div class="form-row card-body">
+                                <div class="form-group col-xl-4">
+                                    <label for="inputNumFacturacion">Numero comprobante</label>
+                                    <input maxLength="20" id="inputNumFacturacion" name="inputNumFacturacion"
+                                        type="number" class="form-control" placeholder="Nº Boleta/Factura" required>
                                 </div>
-                                <h6>Metodo de pago</h6>
-                                <div class="form-row card-body">
-                                    <div class="custom-control custom-radio custom-control-inline ">
-                                        <input type="radio" value=1 id="radioEfectivo" name="customRadio2"
-                                            class="custom-control-input" checked>
-                                        <label class="custom-control-label" for="radioEfectivo">Efectivo</label>
-                                    </div>
-                                    <div class="custom-control custom-radio custom-control-inline ">
-                                        <input type="radio" value=2 id="radioCheque" name="customRadio2"
-                                            class="custom-control-input">
-                                        <label class="custom-control-label" for="radioCheque">Cheque</label>
-                                    </div>
-                                    <div class="custom-control custom-radio custom-control-inline ">
-                                        <input type="radio" value=3 id="radioTarjeta" name="customRadio2"
-                                            class="custom-control-input">
-                                        <label class="custom-control-label" for="radioTarjeta">Tarjeta
-                                            credito/debito</label>
-                                    </div>
-                                    <div class="custom-control custom-radio custom-control-inline ">
-                                        <input type="radio" value=4 id="radioTranferencia" name="customRadio2"
-                                            class="custom-control-input">
-                                        <label class="custom-control-label" for="radioTranferencia">Transferencia
-                                            electronica</label>
-                                    </div>
+                                <div class="form-group col-xl-6">
+                                    <label for="inputFileFacturacion">Comprobante</label>
+                                    <input accept="image/x-png,image/gif,image/jpeg,image/jpg,application/pdf"
+                                        type="file" class="form-control-file" id="inputFileFacturacion"
+                                        name="inputFileFacturacion" required>
                                 </div>
-                                <br>
-                                <div class="form-row card-body">
-                                    <div class="form-group col-xl-6">
-                                        <label for="inputCodigoEmpresaRemplazo">Empresas de reemplazo</label>
-                                        <div class="input-group">
-                                            <select id="inputCodigoEmpresaRemplazo" name="inputCodigoEmpresaRemplazo"
-                                                class="form-control">
-                                            </select>
-                                            <div class="input-group-append">
-                                                <button class="btn btn-outline-secondary" type="button"
-                                                    id="btn_buscarPagoEmpresa">
-                                                    <span class="spinner-border spinner-border-sm" role="status"
-                                                        aria-hidden="true" id="spinner_empresa_remplazo"></span>
-                                                    Buscar pagos pendientes</button>
-                                            </div>
+                            </div>
+                            <h6>Metodo de pago</h6>
+                            <div class="form-row card-body">
+                                <div class="custom-control custom-radio custom-control-inline ">
+                                    <input type="radio" value=1 id="radioEfectivo" name="customRadio2"
+                                        class="custom-control-input" checked>
+                                    <label class="custom-control-label" for="radioEfectivo">Efectivo</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline ">
+                                    <input type="radio" value=2 id="radioCheque" name="customRadio2"
+                                        class="custom-control-input">
+                                    <label class="custom-control-label" for="radioCheque">Cheque</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline ">
+                                    <input type="radio" value=3 id="radioTarjeta" name="customRadio2"
+                                        class="custom-control-input">
+                                    <label class="custom-control-label" for="radioTarjeta">Tarjeta
+                                        credito/debito</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline ">
+                                    <input type="radio" value=4 id="radioTranferencia" name="customRadio2"
+                                        class="custom-control-input">
+                                    <label class="custom-control-label" for="radioTranferencia">Transferencia
+                                        electronica</label>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="form-row card-body">
+                                <div class="form-group col-xl-6">
+                                    <label for="inputCodigoEmpresaRemplazo">Empresas de reemplazo</label>
+                                    <div class="input-group">
+                                        <select id="inputCodigoEmpresaRemplazo" name="inputCodigoEmpresaRemplazo"
+                                            class="form-control">
+                                        </select>
+                                        <div class="input-group-append">
+                                            <button class="btn btn-outline-secondary" type="button"
+                                                id="btn_buscarPagoEmpresa">
+                                                <span class="spinner-border spinner-border-sm" role="status"
+                                                    aria-hidden="true" id="spinner_empresa_remplazo"></span>
+                                                Buscar pagos pendientes</button>
                                         </div>
                                     </div>
-                                    <br>
                                 </div>
-                            </div>
-                            <br><br>
-                            <div class="container">
-                                <table id="tabla_pagoPendienteRemplazo" class="table table-striped table-bordered"
-                                    style="width:100%">
-                                    <thead class="btn-dark">
-                                        <tr>
-                                            <th></th>
-                                            <th>estado</th>
-                                            <th>neto</th>
-                                            <th>iva</th>
-                                            <th>total</th>
-                                            <th>fecha registro</th>
-                                            <th>Nº Arriendo</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody id="vehiculos">
-                                    </tbody>
-                                    <tfoot class="btn-dark">
-                                        <tr>
-                                            <th></th>
-                                            <th>estado</th>
-                                            <th>neto</th>
-                                            <th>iva</th>
-                                            <th>total</th>
-                                            <th>fecha registro</th>
-                                            <th>Nº Arriendo</th>
-                                        </tr>
-                                    </tfoot>
-                                </table>
-                                <br><br>
-                                <button type="submit" id="btn_registrar_facturacion" class="btn btn-success">
-                                    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"
-                                        id="spinner_registrar_facturacion"></span>
-                                    Ingresar facturacion</button>
                                 <br>
                             </div>
+                        </div>
+                        <br><br>
+                        <div class="container">
+                            <table id="tabla_pagoPendienteRemplazo" class="table table-striped table-bordered"
+                                style="width:100%">
+                                <thead class="btn-dark">
+                                    <tr>
+                                        <th></th>
+                                        <th>estado</th>
+                                        <th>neto</th>
+                                        <th>iva</th>
+                                        <th>total</th>
+                                        <th>fecha registro</th>
+                                        <th>Nº Arriendo</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="vehiculos">
+                                </tbody>
+                                <tfoot class="btn-dark">
+                                    <tr>
+                                        <th></th>
+                                        <th>estado</th>
+                                        <th>neto</th>
+                                        <th>iva</th>
+                                        <th>total</th>
+                                        <th>fecha registro</th>
+                                        <th>Nº Arriendo</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                            <br><br>
+                            <button type="submit" id="btn_registrar_facturacion" class="btn btn-success">
+                                <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"
+                                    id="spinner_registrar_facturacion"></span>
+                                Ingresar facturacion</button>
                             <br>
                         </div>
+                        <br>
+
                     </div>
                 </form>
             </div>
