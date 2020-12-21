d1ddf15fe8fd51d6e183788457928fa711152792
diego <d.riosrojas007@gmail.com>
2020-12-20 21:17:35 -0300

    modificaiones

--- application/helpers/urls_helper.php
+++ application/helpers/urls_helper.php
@@ -2,15 +2,22 @@
 
 function api_url()
 {
-    return "http://localhost:3000/rentacar/";
+	return "http://localhost:3000/rentacar/";
 }
 
 function base_route()
 {
-    return "http://localhost/proyectos/rentacar/";
+	return "http://localhost/proyectos/rentacar/";
+}
+
+function client_url()
+{
+	$user = $_SESSION["id"];
+	$token = $_SESSION["usertoken"];
+	return "http://localhost:4200/#/auth/" . $user . "/" . $token;
 }
 
 function path_cert()
 {
-    return false;
+	return false;
 }
