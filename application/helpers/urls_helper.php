c4b953f1b2eecc2778e1a6248058e49ae690ab85
diego rios rojas <d.riosrojas007@gmail.com>
2020-12-29 18:12:04 -0300

    fin jornada

--- application/helpers/urls_helper.php
+++ application/helpers/urls_helper.php
@@ -2,22 +2,21 @@
 
 function api_url()
 {
-	return "http://localhost:3000/rentacar/";
+    return "http://localhost:3000/rentacar/";
 }
 
 function base_route()
 {
-	return "http://localhost/proyectos/rentacar/";
+    return "http://localhost/proyectos/rentacar/";
 }
 
 function client_url()
 {
-	$user = $_SESSION["id"];
-	$token = $_SESSION["usertoken"];
-	return "http://localhost:4200/#/auth/" . $user . "/" . $token;
+    $token = $_SESSION["usertoken"];
+    return "http://localhost:4200/#/auth/" . $token;
 }
 
 function path_cert()
 {
-	return false;
+    return false;
 }
