f3ea26c70cb8c893adab5e29e3c3dac50711b925
diego rios rojas <d.riosrojas007@gmail.com>
2021-01-27 12:06:17 -0300

    ocultar vista

--- application/views/content/sidebars/sidebar_atencion.php
+++ application/views/content/sidebars/sidebar_atencion.php
@@ -9,45 +9,45 @@ $rol = $this->session->userdata("rol");
         <ul class="nav flex-column">
             <br>
             <?php if ($rol == 1 || $rol == 2 || $rol == 3) { ?>
-            <li class="nav-item" id="l_reserva">
-                <br>
-                <a id="m_reserva" class="nav-link" href="<?php echo base_url() ?>modulos_atencion?modulo=4">
-                    <i class="fas fa-calendar-alt"></i>
-                    Calendario de reservas
-                </a>
-                <br>
-            </li>
+                <li hidden class="nav-item" id="l_reserva">
+                    <br>
+                    <a id="m_reserva" class="nav-link" href="<?php echo base_url() ?>modulos_atencion?modulo=4">
+                        <i class="fas fa-calendar-alt"></i>
+                        Calendario de reservas
+                    </a>
+                    <br>
+                </li>
             <?php } ?>
             <?php if ($rol == 1 || $rol == 2 || $rol == 3) { ?>
-            <li class="nav-item" id="l_cliente">
-                <br>
-                <a id="m_cliente" class="nav-link" href="<?php echo base_url() ?>modulos_atencion?modulo=1">
-                    <i class="fas fa-address-book "></i>
-                    gestion de Clientes
-                </a>
-                <br>
-            </li>
+                <li class="nav-item" id="l_cliente">
+                    <br>
+                    <a id="m_cliente" class="nav-link" href="<?php echo base_url() ?>modulos_atencion?modulo=1">
+                        <i class="fas fa-address-book "></i>
+                        gestion de Clientes
+                    </a>
+                    <br>
+                </li>
             <?php } ?>
             <?php if ($rol == 1 || $rol == 2 || $rol == 3) { ?>
-            <li class="nav-item" id="l_arriendo">
-                <br>
-                <a id="m_arriendo" class="nav-link" href="<?php echo base_url() ?>modulos_atencion?modulo=2">
-                    <i class="fas fa-file-signature"></i>
-                    Registro y gestion de Arriendo
-                </a>
-                <br>
-            </li>
+                <li class="nav-item" id="l_arriendo">
+                    <br>
+                    <a id="m_arriendo" class="nav-link" href="<?php echo base_url() ?>modulos_atencion?modulo=2">
+                        <i class="fas fa-file-signature"></i>
+                        Registro y gestion de Arriendo
+                    </a>
+                    <br>
+                </li>
             <?php } ?>
 
             <?php if ($rol == 1 || $rol == 2 || $rol == 3) { ?>
-            <li class="nav-item" id="l_despacho">
-                <br>
-                <a id="m_despacho" class="nav-link" href="<?php echo base_url() ?>modulos_atencion?modulo=3">
-                    <i class="fas fa-concierge-bell"></i>
-                    Gestion despacho y recepcion
-                </a>
-                <br>
-            </li>
+                <li class="nav-item" id="l_despacho">
+                    <br>
+                    <a id="m_despacho" class="nav-link" href="<?php echo base_url() ?>modulos_atencion?modulo=3">
+                        <i class="fas fa-concierge-bell"></i>
+                        Gestion despacho y recepcion
+                    </a>
+                    <br>
+                </li>
             <?php } ?>
 
 
