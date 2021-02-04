e89e3290142d92749325e3d3e7d10b822ce48d9a
diego rios rojas <d.riosrojas007@gmail.com>
2021-02-04 09:31:54 -0300

    confir

--- application/views/content/view_module/views_gestion/view_pagoCliente/view_tabla_pagoCliente.php
+++ application/views/content/view_module/views_gestion/view_pagoCliente/view_tabla_pagoCliente.php
@@ -10,10 +10,58 @@
         <h1 class="h3">Gestion de Pagos clientes</h1>
     </div>
     <div>
-
-        <h6>contenido..</h6>
-
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <br>
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <button class="btn btn-outline-secondary" ype="button">Buscar pagos </button>
+                    </div>
+                    <input type="number" #inputBuscar class="form-control" placeholder="ingrese Nº de Folio del arriendo" aria-label="Example text with button addon" aria-describedby="button-addon1">
+                </div>
+            </div>
+            <div class="col-md-3"></div>
+        </div>
     </div>
+    <br>
+    <div class="scroll">
+        <table id="tabla_pagosCliente" class="table table-striped table-bordered" style="width:100%">
+            <thead class="btn-dark">
+                <tr>
+                    <th>dias</th>
+                    <th>estado pago</th>
+                    <th>neto</th>
+                    <th>iva</th>
+                    <th>total</th>
+                    <th>fecha registro</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody id="vehiculos">
+                <br>
+            </tbody>
+            <tfoot class="btn-dark">
+                <tr>
+                    <th>dias</th>
+                    <th>estado pago</th>
+                    <th>neto</th>
+                    <th>iva</th>
+                    <th>total</th>
+                    <th>fecha registro</th>
+                    <th></th>
+                </tr>
+            </tfoot>
+        </table>
+    </div>
+    <div class="text-center" id="spinner_tabla_pagos">
+        <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        <h6>Cargando Datos...</h6>
+    </div>
+
+    <br><br><br><br>
 </main>
 </div>
 </div>
