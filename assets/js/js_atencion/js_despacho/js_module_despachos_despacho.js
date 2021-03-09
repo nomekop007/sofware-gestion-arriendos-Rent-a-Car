2a3cd47682f84565b6dfecf127a2d0328a7cb6e2
diego rios rojas <d.riosrojas007@gmail.com>
2021-03-09 16:54:26 -0300

    correciones

--- assets/js/js_atencion/js_despacho/js_module_despachos_despacho.js
+++ assets/js/js_atencion/js_despacho/js_module_despachos_despacho.js
@@ -115,7 +115,7 @@ $(document).ready(() => {
 		if (inputImg != 0) {
 			const canvas = document.getElementById("canvas-fotoVehiculo");
 			const base64 = canvas.toDataURL("image/png");
-			const url = await resizeBase64Img(base64, canvas.width, canvas.height, 1);
+			const url = await resizeBase64Img(base64, canvas.width, canvas.height, 2);
 			if (arrayImages.length > 9) {
 				Swal.fire({ icon: "warning", title: "el maximo son 10 imagenes", });
 				return;
